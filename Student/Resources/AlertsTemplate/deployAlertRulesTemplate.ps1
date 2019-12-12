47cc172172f81a97996e18764616014b2e114e7c
Maxim Sergeev <maserg@microsoft.com>
2019-12-12 16:19:12 +0300

    updates to Az module

--- Student/Resources/AlertsTemplate/deployAlertRulesTemplate.ps1
+++ Student/Resources/AlertsTemplate/deployAlertRulesTemplate.ps1
@@ -1,11 +1,11 @@
-Connect-AzureRmAccount
+Connect-AzAccount
 
 #Specify your resourcegroup
 $rgname=""
-$rg = Get-AzureRmResourceGroup -Name $rgname
+$rg = Get-AzResourceGroup -Name $rgname
 
 #Get Azure Monitor Action Group
-(Get-AzureRmResource -ResourceType 'Microsoft.Insights/actiongroups').ResourceId
+(Get-AzResource -ResourceType 'Microsoft.Insights/actiongroups').ResourceId
 
 #Update Path to files as needed
 #Update the parameters file with the names of your VMs and the ResourceId of your Action Group (use command above to find ResourceId)
@@ -13,7 +13,7 @@ $template=".\AlertsTemplate\GenerateAlertRules.json"
 $para=".\AlertsTemplate\deployAlertRules.parameters.json"
 
 $job = 'job.' + ((Get-Date).ToUniversalTime()).tostring("MMddyy.HHmm")
-New-AzureRmResourceGroupDeployment `
+New-AzResourceGroupDeployment `
   -Name $job `
   -ResourceGroupName $rg.ResourceGroupName `
   -TemplateFile $template `
@@ -22,7 +22,7 @@ New-AzureRmResourceGroupDeployment `
 
 #Note: At the time I created this, the PowerShell cmdlet was targeting the wrong resourceType and is scheduled to be updated
 #To check your results - Get metrixAlerts Rule for Resourcegroup
-Get-AzureRmResource -ResourceGroupName $rg.ResourceGroupName -ResourceType 'Microsoft.Insights/metricalerts' -Name CPU*| ft
+Get-AzResource -ResourceGroupName $rg.ResourceGroupName -ResourceType 'Microsoft.Insights/metricalerts' -Name CPU*| ft
 
 #To delete your Alert Rules
-Get-AzureRmResource -ResourceGroupName $rg.ResourceGroupName -ResourceType 'Microsoft.Insights/metricalerts'  -Name CPU* | Remove-AzureRmResource -Force
+Get-AzResource -ResourceGroupName $rg.ResourceGroupName -ResourceType 'Microsoft.Insights/metricalerts'  -Name CPU* | Remove-AzResource -Force
