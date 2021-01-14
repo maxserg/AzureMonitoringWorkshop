e3d333e4be970dae90de029c7b869a2bf96ac81d
Maxim Sergeev <maserg@microsoft.com>
2021-01-14 13:11:16 +0300

    updated content

--- Student/Resources/ARM-Examples/Automation-Account/scripts/how-to-start-via-webhook.ps1
+++ Student/Resources/ARM-Examples/Automation-Account/scripts/how-to-start-via-webhook.ps1
@@ -1,5 +1,7 @@
-$uri = "https://9ac0f748-d067-48aa-a9e1-10fbcc2d9977.webhook.eus2.azure-automation.net/webhooks?token=FRGBmp62ydq%2bCPIuUL%2fna3lsfkC3dxphOjtn9v4kKwA%3d"
-$vm  = "test-vm01"
+$uri = ""
+$vm  = @(
+            @{ vmname="test-vm01"}
+        )
 $body = ConvertTo-Json -InputObject $vm
 $header = @{ message="StartedbyContoso"}
 $response = Invoke-WebRequest -Method Post -Uri $uri -Body $body -Headers $header
